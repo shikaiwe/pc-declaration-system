a907ece0a7e90f0cd77996a1c88f8ece910fc7b7
QiannanYou <2102202153@qq.com>
2026-04-02 18:47:49 +0800

    fix(csrf): 修正CSRF API URL缺少结尾斜杠的问题

--- src/js/csrf.js
+++ src/js/csrf.js
@@ -15,7 +15,7 @@
     const CSRF_CONFIG = {
         CSRF_COOKIE_NAME: 'csrftoken',
         CSRF_HEADER_NAME: 'X-CSRFToken',
-        CSRF_API_URL: 'api/unit/csrf'
+        CSRF_API_URL: 'api/unit/csrf/'
     };
 
     /**
